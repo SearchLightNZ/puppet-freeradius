810a32d74055f5d72acf51624643671beb64d273
Nathan Ward <nward@searchlight.nz>
2021-05-07 00:59:32 +1200

    Fix up freeradius_version spec
    It was trying to load the osfamily spec. Setting the facts hash (i.e. `let(:facts)`) doesn't mock `Facter.value(:osfamily)` etc.

--- spec/unit/facter/freeradius_version_spec.rb
+++ spec/unit/facter/freeradius_version_spec.rb
@@ -2,24 +2,32 @@ require 'spec_helper'
 require 'facter/freeradius_version'
 
 describe 'freeradius_version', type: :fact do
-  before :each do
-    Facter.clear
-    orig_exec_method = Facter::Core::Execution.method(:exec)
-    allow(Facter::Core::Execution).to receive(:exec) do |cmd|
-      case cmd
-      when 'radiusd -v'
-        'FreeRADIUS Version 3.0.21'
-      else
-        orig_exec_method.call(cmd)
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      before :each do
+        Facter.clear
+        orig_exec_method = Facter::Core::Execution.method(:exec)
+        allow(Facter).to receive(:value).with(:osfamily).and_return(facts[:osfamily])
+        allow(Facter).to receive(:value).with(:kernel).and_return(facts[:kernel])
+        allow(Facter::Core::Execution).to receive(:exec) do |cmd|
+          case cmd
+          when %r{^(radiusd|freeradius) -v$}
+            'FreeRADIUS Version 3.0.21'
+          else
+            orig_exec_method.call(cmd)
+          end
+        end
+      end
+
+      it 'sets freeradius_version' do
+        expect(Facter.fact(:freeradius_version).value).to eq('3.0.21')
+      end
+
+      it 'sets freeradius_maj_version' do
+        expect(Facter.fact(:freeradius_maj_version).value).to eq('3')
       end
     end
-  end
-
-  it 'sets freeradius_version' do
-    expect(Facter.fact(:freeradius_version).value).to eq('3.0.21')
-  end
-
-  it 'sets freeradius_maj_version' do
-    expect(Facter.fact(:freeradius_maj_version).value).to eq('3')
   end
 end
